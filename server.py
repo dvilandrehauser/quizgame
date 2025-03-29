672932b5368080065011282ee4fc0ba7778daa86
dvilandrehauser <dvilandrehauser@mail.niagara.edu>
2025-03-29 21:41:48 +0000

    added facehooked users list to status.html file

--- server.py
+++ server.py
@@ -1,28 +1,12 @@
 # QUIZGAME SERVER
-import socket
 
 print("starting derwin's quizgame server")
-
-# receive a UDP packet
-
-s = socket.socket(type=socket.SOCK_DGRAM)
-s.bind(('0.0.0.0', 15000))
-payload, addr = s.recvfrom(4096)
-print('PAYLOAD:', payload.decode())
-print('ADDR:', addr)
-s.close()
-
 
 
 
 file = open('status.html', 'w')
-file.write('QUIZGAME STATUS')
+file.write('FACEHOOKED USERS LIST:\n')
 file.close()
-
-
-
-
-
 
 
 
